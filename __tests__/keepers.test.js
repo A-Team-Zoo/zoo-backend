db8845d7e7f2c20ba01fb41296893a1ff0afd50b
Andrea Cleland <andreaclelan@gmail.com>
2022-06-29 16:16:49 -0700

    added a test for a mock user with incorrect email, test passes

--- __tests__/keepers.test.js
+++ __tests__/keepers.test.js
@@ -9,6 +9,12 @@ const mockKeeper = {
   password: '123456',
 };
 
+const mockPretender = {
+  name: 'Andria',
+  email: 'andria@notzoo.com',
+  password: '654321',
+};
+
 describe('keepers auth tests', () => {
   beforeEach(() => {
     return setup(pool);
@@ -22,6 +28,12 @@ describe('keepers auth tests', () => {
       email,
       name,
     });
+  });
+
+  it('should reject someone who does not have @zoo.com email', async () => {
+    const res = await request(app).post('/api/v1/keepers').send(mockPretender);
+    expect(res.status).toBe(500);
+    expect(res.body.message).toEqual('REJECTED!!! You are not a Keeper');
   });
 
   afterAll(() => {
