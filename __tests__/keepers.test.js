fda39daa44fcea4b4c3dfc52e47a85edad8a49cb
aidanliddiard <aidanliddiard@gmail.com>
2022-06-29 16:51:25 -0700

    setup signin test

--- __tests__/keepers.test.js
+++ __tests__/keepers.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const KeeperService = require('../lib/services/KeeperService');
 
 const mockKeeper = {
   name: 'Andrea',
@@ -30,10 +31,35 @@ describe('keepers auth tests', () => {
     });
   });
 
+  const registerAndLogin = async (keeperProps = {}) => {
+    const password = keeperProps.password ?? mockKeeper.password;
+
+    const agent = request.agent(app);
+
+    const keeper = await KeeperService.create({
+      ...mockKeeper,
+      ...keeperProps,
+    });
+
+    const { email } = keeper;
+    await agent.post('/api/vi/keepers/sessions').send({ email, password });
+    return [agent, keeper];
+  };
+
   it('should reject someone who does not have @zoo.com email', async () => {
     const res = await request(app).post('/api/v1/keepers').send(mockPretender);
     expect(res.status).toBe(500);
     expect(res.body.message).toEqual('REJECTED!!! You are not a Keeper');
+  });
+
+  it('should return the current user', async () => {
+    const [agent, keeper] = await registerAndLogin();
+    const me = await agent.get('/api/v1/keepers/me');
+    expect(me.body).toEqual({
+      ...keeper,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
   });
 
   afterAll(() => {
