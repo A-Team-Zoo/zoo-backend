2e3f27ad63af0c68609565772ff67bd26befc44b
Andrea Cleland <andreaclelan@gmail.com>
2022-06-29 16:09:45 -0700

    create keeper test passing

--- __tests__/keepers.test.js
+++ __tests__/keepers.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const mockKeeper = {
+  name: 'Andrea',
   email: 'andrea@zoo.com',
   password: '123456',
 };
@@ -14,11 +15,12 @@ describe('keepers auth tests', () => {
   });
   it('should create a new user', async () => {
     const res = await request(app).post('/api/v1/keepers').send(mockKeeper);
-    const { email } = mockKeeper;
+    const { email, name } = mockKeeper;
 
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
+      name,
     });
   });
 
