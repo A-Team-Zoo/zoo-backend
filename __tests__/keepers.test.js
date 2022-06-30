4dd4c3f66e07a3846de908a4fea4f8b9ec9fb11a
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-29 17:51:19 -0700

    added delete test and delete to controller

--- __tests__/keepers.test.js
+++ __tests__/keepers.test.js
@@ -63,6 +63,12 @@ describe('keepers auth tests', () => {
     });
   });
 
+  it('Should sign out user', async () => {
+    const [agent] = await registerAndLogin();
+    const resp = await agent.delete('/api/v1/keepers/sessions');
+    expect(resp.status).toBe(204);
+  });
+
   afterAll(() => {
     pool.end();
   });
