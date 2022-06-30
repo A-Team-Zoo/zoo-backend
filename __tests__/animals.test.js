966440d3a30913c8f9cb1a69b97b0f48c1dbd421
aidanliddiard <aidanliddiard@gmail.com>
2022-06-30 12:28:27 -0700

    added create route and test passing

--- __tests__/animals.test.js
+++ __tests__/animals.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const KeeperService = require('../lib/services/KeeperService');
 
 const animals = [
   {
@@ -29,6 +30,32 @@ const animals = [
       'https://images.unsplash.com/photo-1598439210625-5067c578f3f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVuZ3VpbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
   },
 ];
+const mockKeeper = {
+  name: 'Andrea',
+  email: 'andrea@zoo.com',
+  password: '123456',
+};
+
+const mockPretender = {
+  name: 'Andria',
+  email: 'andria@notzoo.com',
+  password: '654321',
+};
+
+const registerAndLogin = async (keeperProps = {}) => {
+  const password = keeperProps.password ?? mockKeeper.password;
+
+  const agent = request.agent(app);
+
+  const keeper = await KeeperService.create({
+    ...mockKeeper,
+    ...keeperProps,
+  });
+
+  const { email } = keeper;
+  await agent.post('/api/v1/keepers/sessions').send({ email, password });
+  return [agent, keeper];
+};
 
 describe('backend animal routes', () => {
   beforeEach(() => {
@@ -46,6 +73,21 @@ describe('backend animal routes', () => {
     const res = await request(app).get('/api/v1/animals/3');
     expect(res.status).toEqual(200);
     expect(res.body).toEqual(animals[2]);
+  });
+  it('should allow keepers to create new animals', async () => {
+    const newSeal = {
+      name: 'Seal',
+      image:
+        'https://images.unsplash.com/photo-1565413294262-fa587c396965?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2VhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    };
+    const [agent] = await registerAndLogin();
+    const res = await agent.post('/api/v1/animals').send(newSeal);
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: newSeal.name,
+      image: newSeal.image,
+    });
   });
 
   afterAll(() => {
